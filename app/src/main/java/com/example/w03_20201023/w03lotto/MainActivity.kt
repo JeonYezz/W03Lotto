0bb293b9e81ea61c581737d9a98a936f0c284015
JunYezz <jyj1055@kumoh.ac.kr>
2023-09-19 12:19:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/w03_20201023/w03lotto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.w03_20201023.w03lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
